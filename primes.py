24a40a21d966470ae6124ed1e72022a7efa0009c
dominicchildrv <114876361+dominicchildrv@users.noreply.github.com>
2022-10-07 15:16:28 +0100

    Solution complete

--- primes.py
+++ primes.py
@@ -1,6 +1,30 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
 
 def primes(number_of_primes):
     list = []
+    count = 1
+    currentNumber = 2
+
+    if number_of_primes < 1:
+        raise ValueError("Input number has to be positive and non zero")
+
+    while count < (number_of_primes) + 1:
+        isPrime = True
+        for i in range (2,currentNumber + 1):
+            if i == currentNumber:
+                pass
+            else:
+                if currentNumber%i == 0:
+                    isPrime = False
+        if isPrime == True:
+            count = count + 1
+            list.append(currentNumber)
+            currentNumber = currentNumber + 1
+
+        else:
+            currentNumber = currentNumber + 1
+
+
     return list
